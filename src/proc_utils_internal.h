5981d0b4780bf49abed27e1facdbc8738558a707
daiaji <hentaiwushuang@gmail.com>
2025-11-13 00:26:46 +0800

    静态链接

--- src/proc_utils_internal.h
+++ src/proc_utils_internal.h
@@ -4,15 +4,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <windows.h>
+
+#include <atomic>
 #include <psapi.h>
 #include <tlhelp32.h>
-#include <atomic>
 
 // RAII 包装器，用于自动管理 HANDLE
 class ScopedHandle {
-public:
-    explicit ScopedHandle(HANDLE h = INVALID_HANDLE_VALUE) : handle_(h) {}
-    ~ScopedHandle() {
+ public:
+    explicit ScopedHandle(HANDLE h = INVALID_HANDLE_VALUE) : handle_(h)
+    {
+    }
+    ~ScopedHandle()
+    {
         if (handle_ != INVALID_HANDLE_VALUE && handle_ != NULL) {
             ::CloseHandle(handle_);
         }
@@ -23,22 +27,31 @@ public:
     ScopedHandle& operator=(const ScopedHandle&) = delete;
 
     // 允许移动
-    ScopedHandle(ScopedHandle&& other) noexcept : handle_(other.handle_) {
+    ScopedHandle(ScopedHandle&& other) noexcept : handle_(other.handle_)
+    {
         other.handle_ = INVALID_HANDLE_VALUE;
     }
-    ScopedHandle& operator=(ScopedHandle&& other) noexcept {
+    ScopedHandle& operator=(ScopedHandle&& other) noexcept
+    {
         if (this != &other) {
-            if (handle_ != INVALID_HANDLE_VALUE && handle_ != NULL) ::CloseHandle(handle_);
+            if (handle_ != INVALID_HANDLE_VALUE && handle_ != NULL)
+                ::CloseHandle(handle_);
             handle_ = other.handle_;
             other.handle_ = INVALID_HANDLE_VALUE;
         }
         return *this;
     }
 
-    operator HANDLE() const { return handle_; }
-    bool IsValid() const { return handle_ != INVALID_HANDLE_VALUE && handle_ != NULL; }
+    operator HANDLE() const
+    {
+        return handle_;
+    }
+    bool IsValid() const
+    {
+        return handle_ != INVALID_HANDLE_VALUE && handle_ != NULL;
+    }
 
-private:
+ private:
     HANDLE handle_;
 }; // <--- 修正：添加了分号
 
@@ -48,16 +61,14 @@ extern std::atomic<bool> g_procutils_should_exit;
 // 极简的非阻塞等待函数
 void ProcUtils_MsgWait(int duration_ms);
 
-
 // 内部实现函数的命名空间
-namespace ProcUtils::Internal
-{
-    DWORD FindProcess(const wchar_t* process_name_or_pid);
+namespace ProcUtils::Internal {
+DWORD FindProcess(const wchar_t* process_name_or_pid);
 
-    DWORD GetProcessPath(DWORD process_id, wchar_t* buffer, DWORD buffer_size);
+DWORD GetProcessPath(DWORD process_id, wchar_t* buffer, DWORD buffer_size);
 
-    DWORD GetParentProcessId(DWORD child_pid);
+DWORD GetParentProcessId(DWORD child_pid);
 
-    bool WaitForProcess(const wchar_t* process_name_or_pid, int timeout_ms, bool wait_for_close, DWORD* out_pid);
+bool WaitForProcess(const wchar_t* process_name_or_pid, int timeout_ms, bool wait_for_close, DWORD* out_pid);
 
 } // namespace ProcUtils::Internal
